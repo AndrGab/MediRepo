acc2f0503b8640855be1a92ee7d154853049dc7a
andrgab <andrgab@gmail.com>
2021-05-01 13:50:04 -0300

    UUID checker included

---
+++ lib/medirepo_web/plugs/uuid_checker.ex
@@ -0,0 +1,26 @@
+defmodule MedirepoWeb.Plugs.UUIDChecker do
+  import Plug.Conn
+
+  alias Ecto.UUID
+  alias Plug.Conn
+
+  def init(options), do: options
+
+  def call(%Conn{params: %{"id" => id}} = conn, _opts) do
+    case UUID.cast(id) do
+      :error -> render_error(conn)
+      {:ok, _uuid} -> conn
+    end
+  end
+
+  def call(conn, _opts), do: conn
+
+  defp render_error(conn) do
+    body = Jason.encode!(%{message: "Invalid UUID"})
+
+    conn
+    |> put_resp_content_type("application/json")
+    |> send_resp(:bad_request, body)
+    |> halt()
+  end
+end
