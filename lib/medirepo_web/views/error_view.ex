b5d7c1d3fa848f725b2c3de4ef092329e5bd9d64
andrgab <andrgab@gmail.com>
2021-05-01 14:26:49 -0300

    update

--- lib/medirepo_web/views/error_view.ex
+++ lib/medirepo_web/views/error_view.ex
@@ -14,6 +14,7 @@ defmodule MedirepoWeb.ErrorView do
   # By default, Phoenix returns the status message from
   # the template name. For example, "404.json" becomes
   # "Not Found".
+
   def template_not_found(template, _assigns) do
     %{errors: %{detail: Phoenix.Controller.status_message_from_template(template)}}
   end
