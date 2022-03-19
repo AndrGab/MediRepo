746dc0d210247f081b51b081c1806fefd957f0c9
andrgab <andrgab@gmail.com>
2022-03-19 11:44:18 -0300

    fix: test production

--- lib/medirepo_web/controllers/redirect_controller.ex
+++ lib/medirepo_web/controllers/redirect_controller.ex
@@ -2,6 +2,9 @@ defmodule MedirepoWeb.RedirectController do
   use MedirepoWeb, :controller
 
   def index(conn, _params) do
+    IO.inspect(System.get_env("SMTP_USERNAME"))
+    IO.inspect(System.get_env("SMTP_PASSWORD"))
+
     conn
     |> put_status(:ok)
     |> text("https://github.com/AndrGab/MediRepo")
