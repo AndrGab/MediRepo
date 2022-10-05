9b63ee5d085992b6e46a05ae1bbcf43cc5b8fc71
Andre Gabriel <andrgab@gmail.com>
2022-10-05 09:50:29 -0300

    fix: migrations (#23)

--- priv/repo/migrations/20210501003852_create_bulletins_table.exs
+++ priv/repo/migrations/20210501003852_create_bulletins_table.exs
@@ -3,19 +3,19 @@ defmodule Medirepo.Repo.Migrations.CreateBulletinsTable do
 
   def change do
     create table(:bulletins) do
-      add :name, :string
-      add :dt_birth, :date
-      add :general, :string
-      add :pressure, :string
-      add :conscience, :string
-      add :fever, :string
-      add :respiration, :string
+      add :nome, :string
+      add :dt_nascimento, :date
+      add :geral, :string
+      add :pressao, :string
+      add :consciencia, :string
+      add :febre, :string
+      add :respiracao, :string
       add :diurese, :string
-      add :notes, :string
-      add :doctor, :string
-      add :dt_signature, :date
-      add :attendance, :integer
-      add :cd_patient, :integer
+      add :obs, :string
+      add :medico, :string
+      add :dt_assinatura, :date
+      add :atendimento, :integer
+      add :cd_paciente, :integer
       add :hospital_id, references(:hospitals, type: :binary_id)
 
       timestamps()
