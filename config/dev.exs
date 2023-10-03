0a9911ea23f07f077c3b78210713c69101206002
Andre Gabriel <andrgab@gmail.com>
2023-10-03 01:30:42 -0300

    fix: credo

--- config/dev.exs
+++ config/dev.exs
@@ -2,8 +2,8 @@ import Config
 
 # Configure your database
 config :medirepo, Medirepo.Repo,
-  username: "solfacil",
-  password: "solfacil",
+  username: "postgres",
+  password: "postgres",
   database: "medirepo_dev",
   hostname: "localhost",
   show_sensitive_data_on_connection_error: true,
