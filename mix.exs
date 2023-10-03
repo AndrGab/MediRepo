24871150722ad3f1102c826fd8c176908dff5fa4
Andre Gabriel <andrgab@gmail.com>
2023-10-03 01:49:36 -0300

    fix: ci (#38)
    * fix: ci
    
    * fix: typo
    
    * fix: credo
    
    * updates coverall

--- mix.exs
+++ mix.exs
@@ -51,7 +51,7 @@ defmodule Medirepo.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
       {:pbkdf2_elixir, "~> 1.4"},
       {:guardian, "~> 2.0"},
       {:secure_random, "~> 0.5"},
